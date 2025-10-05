b033603c508b6fd1987c037ddb53903de272d70b
Andriy <andrijs373@gmail.com>
2025-10-05 12:47:47 +0300

    add task solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,17 +1,20 @@
-export const Person = ({ person }) => (
-  <div className="Person">
-    <div className="Person__name">{person.name}</div>
-    {person.age !== undefined && (
-      <div className="Person__age">{person.age}</div>
-    )}
-    {person.married ? (
-      <div className="Person__partner">
-        {person.gender === 'male'
-          ? `wife: ${person.partner}`
-          : `husband: ${person.partner}`}
-      </div>
-    ) : (
-      <div className="Person__partner">I am not married</div>
-    )}
-  </div>
-);
+export const Person = ({ person }) => {
+  const { name, age, married, gender, partner } = person;
+  let partnerLine = null;
+
+  if (married) {
+    const partnerLabel = gender === 'male' ? 'wife' : 'husband';
+
+    partnerLine = `${partnerLabel}: ${partner}`;
+  } else {
+    partnerLine = 'I am not married';
+  }
+
+  return (
+    <div className="Person">
+      <div className="Person__name">{name}</div>
+      {age !== undefined && <div className="Person__age">{age}</div>}
+      <div className="Person__partner">{partnerLine}</div>
+    </div>
+  );
+};
